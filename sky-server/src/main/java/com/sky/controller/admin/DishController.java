ba266f7dd1d8d42ae6d6cd214dc4459e6358beb4
zebram <zhuoyuhang_1024@foxmail.com>
2023-12-15 21:09:56 +0800

    菜品起售停售业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -101,5 +101,17 @@ public class DishController {
         return Result.success(list);
     }
 
-
+    /**
+     * 菜品起售停售
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("菜品起售停售")
+    public Result startOrStop(@PathVariable Integer status, Long id) {
+        log.info("菜品起售停售:{},{}",status,id);
+        dishService.startOrStop(status, id);
+        return Result.success();
+    }
 }
